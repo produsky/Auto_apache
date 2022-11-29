0a55cfb5af1635ec7d8a93ecec69d2d85d53d095
root <root@vps.server.local>
2022-11-29 23:52:25 +0000

    v1

--- setup_config.sh
+++ setup_config.sh
@@ -1,5 +1,18 @@
 config_file=$1.conf
-touch $config_file
+config_path=/etc/apache2/sites-available/
 
 #Create file in
-echo -e "<VirtualHost *:80> \n ServerAdmin $2\n ServerName $1\n ServerAlias www.$1\n DocumentRoot /var/www/$1/public_html\n</VirtualHost>" > $config_file
+echo -e "<VirtualHost *:80> \n ServerAdmin produskycorp@gmail.com\n ServerName $1\n ServerAlias www.$1\n DocumentRoot /var/www/$1/public_html\n</VirtualHost>" > $config_path$config_file
+echo "File configured"
+
+mkdir /var/www/$1
+mkdir /var/www/$1/public_html
+echo "Document root created"
+
+a2ensite $1
+
+systemctl reload apache2
+echo "systemctl reloaded"
+
+echo "You can now deploy on"
+echo "/var/www/$1/public_html"
